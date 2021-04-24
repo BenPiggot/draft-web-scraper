d53a4694b2421cf3985800da65c045c4cecdb47c
ben <wbpiggot@gmail.com>
2021-04-24 14:20:37 -0700

    adding api gateway to serverless

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import boto3
 from boto3.dynamodb.conditions import Key
 
 def fetch_by_position(position: str):
+    """ fetch all players drafted at a given mock draft position """
     dynamodb_client = boto3.resource('dynamodb', region_name='us-west-2')
     table = dynamodb_client.Table('nfl-mock-drafts-2021')
 
--- handler.py
+++ handler.py
@@ -3,24 +3,11 @@ import app
 
 
 def fetch_by_position(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    players = app.fetch_by_position('12')
+    """ fetch all players drafted at a given mock draft position """
+    players = app.fetch_by_position(str(event['pathParameters']['draft_position']))
 
     response = {
-        "statusCode": 200,
+        "statusCode": 200   ,
         "body": json.dumps(players)
     }
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
