a0cc1b2f3b204b7bd4580bd03f600985f941d1fa
ben <wbpiggot@gmail.com>
2021-04-24 20:28:13 -0700

    updates

--- draft-web-scraper-ui/src/pages/index.js
+++ draft-web-scraper-ui/src/pages/index.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import highcarts from 'highcharts';
 
 
 export default function Home() {
-  return <h1>Hello Gatsby!</h1>
+  const [players, setPlayers] = useState([])
+  const [draftPosition, setDraftPosition] = useState()
+
+  const getPlayersByPostiion = async () => {
+    
+    const response = await fetch(`https://w8a2bosl9k.execute-api.us-west-2.amazonaws.com/dev/draft/${draftPosition}`);
+    const json = await response.json();
+
+    setPlayers(json['body']);
+  }
+
+  const handleSetDraftPosition = (e) => {
+    setDraftPosition(e.target.value)
+  }
+
+
+  useEffect(() => {
+
+  })
+
+  return (
+    <div>
+      <input onChange={handleSetDraftPosition} value={draftPosition} onBlur={getPlayersByPostiion}/>
+      {
+        players.length ? players.map(player => {
+          return (
+            <div>
+              {player.player_name}
+            </div>
+          )
+        }) : null
+      }
+    </div>
+  )
 }
--- handler.py
+++ handler.py
@@ -4,10 +4,15 @@ import app
 
 def fetch_by_position(event, context):
     """ fetch all players drafted at a given mock draft position """
-    players = app.fetch_by_position(str(event['pathParameters']['draft_position']))
+    print(event)
+    players = app.fetch_by_position(str(event['path']['draft_position']))
 
     response = {
-        "statusCode": 200   ,
-        "body": json.dumps(players)
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True
+        },
+        "body": players
     }
     return response
