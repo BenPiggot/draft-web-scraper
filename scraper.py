fa3451de47b68b2767835af71e1b15bbda499d83
ben <wbpiggot@gmail.com>
2021-04-25 22:58:53 -0700

    wip

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ import re
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
-# options.add_argument('--incognito')
+options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 
